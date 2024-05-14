72b3857d82297929f787f1fe42bff34185e55588
Vitor Sampaio <vitor-sampaio@outlook.com.br>
2024-05-14 12:45:11 +0100

    W01 Prove: Developer—C# Programming

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int magicNumber = GenerateMagicNumber();
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            guess = GetUserGuess();
+
+            EvaluateGuess(guess, magicNumber);
+        }
+    }
+
+    static int GenerateMagicNumber()
+    {
+        Random randomGenerator = new Random();
+        return randomGenerator.Next(1, 101);
+    }
+
+    static int GetUserGuess()
+    {
+        Console.Write("What is your guess? ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static void EvaluateGuess(int guess, int magicNumber)
+    {
+        if (magicNumber > guess)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (magicNumber < guess)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,66 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = GetUserNumbers();
+        
+        int sum = ComputeSum(numbers);
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ComputeAverage(sum, numbers.Count);
+        Console.WriteLine($"The average is: {average}");
+
+        int max = FindMax(numbers);
+        Console.WriteLine($"The max is: {max}");
+    }
+
+    static List<int> GetUserNumbers()
+    {
+        List<int> numbers = new List<int>();
+        int userNumber = -1;
+
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        return numbers;
+    }
+
+    static int ComputeSum(List<int> numbers)
+    {
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        return sum;
+    }
+
+    static float ComputeAverage(int sum, int count)
+    {
+        return ((float)sum) / count;
+    }
+
+    static int FindMax(List<int> numbers)
+    {
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        return max;
     }
 }
